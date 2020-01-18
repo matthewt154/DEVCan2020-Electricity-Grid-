6663b9f5ce335b6cb30c00b0031ca67d6abd62f8
Iyiola Aloko <iyi.aloko@gmail.com>
2020-01-18 14:27:19 -0500

    Update MainScrapeScriptElectricLoad.py

--- MainScrapeScriptElectricLoad.py
+++ MainScrapeScriptElectricLoad.py
@@ -3,17 +3,28 @@ import bs4
 import os
 #from apscheduler.schedulers 
 
-def scrapeAlberta:
+def scrapeAlberta():
+	with open("http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet") as fp:
+		soup = BeautifulSoup(fp)
+	id_soup.p.get_attribute_list('td')
 	return 0
 
-def scrapeOntario:
+def scrapeOntario():
 	return 0
 
-def scrapeNB:
+def scrapeNB():
 	return 0
 
-def scrapeNS:
+def scrapeNS():
 	return 0
 
-def mainRun:
+def mainRun():
 	return 0
+
+
+with open("http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet") as fp:
+		soup = BeautifulSoup(fp)
+		Alberta_value = id_soup.p.get_attribute_list('td')
+		print(Alberta_value)
+
+
