01dea21d584b943b212ae1760ad6217a95cbbff7
Iyiola Aloko <iyi.aloko@gmail.com>
2020-01-18 17:46:16 -0500

    Update MainScrapeScriptElectricLoad.py

--- MainScrapeScriptElectricLoad.py
+++ MainScrapeScriptElectricLoad.py
@@ -1,30 +1,42 @@
-import requests 
-import bs4 
+from bs4 import BeautifulSoup
+import requests
 import os
 #from apscheduler.schedulers 
+import lxml.html as lh
 
-def scrapeAlberta():
-	with open("http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet") as fp:
-		soup = BeautifulSoup(fp)
-	id_soup.p.get_attribute_list('td')
-	return 0
+page = requests.get("http://reports.ieso.ca/public/GenOutputbyFuelHourly/")
+tree = lh.fromstring(page.content)
 
-def scrapeOntario():
-	return 0
-
-def scrapeNB():
-	return 0
-
-def scrapeNS():
-	return 0
-
-def mainRun():
-	return 0
+td_elements = tree.xpath('//td/text()')
+l_td = []
+for t in td_elements: 
+        l_td.append(t)
 
 
-with open("http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet") as fp:
-		soup = BeautifulSoup(fp)
-		Alberta_value = id_soup.p.get_attribute_list('td')
-		print(Alberta_value)
 
+for i in range(len(l_td) - 1):
+        if l_td[i] == 'Alberta Internal Load (AIL)':
+                alberta_value = l_td[i + 1]
+       
+print(alberta_value)
 
+#print("Checking for where last update is:")
+#print(l_td)
+
+print("See if this gives last update value")
+for i in range(len(l_td) - 1):
+        if "Last Update" in l_td[i]:
+                format_last_update = l_td[i]
+                
+full_date_of_last_update = format_last_update.split()
+last_update_month = full_date_of_last_update[3]
+last_update_date = full_date_of_last_update[4]
+last_update_year = full_date_of_last_update[5]
+last_update_time = full_date_of_last_update[6]
+
+if "," in last_update_date:
+        last_update_date = last_update_date.replace(","," ")
+
+#print(full_date_of_last_update)
+
+print(last_update_month,last_update_date,last_update_year,last_update_time)
