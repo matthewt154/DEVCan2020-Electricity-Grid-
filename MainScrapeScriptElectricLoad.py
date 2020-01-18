db4bca5a1348902a40ac599e7ffb82a01ef7ae3e
Matthew Tran <mtran014@uottawa.ca>
2020-01-18 14:10:10 -0800

    links updated

--- MainScrapeScriptElectricLoad.py
+++ MainScrapeScriptElectricLoad.py
@@ -3,10 +3,16 @@ import bs4
 import os
 #from apscheduler.schedulers 
 
-data_sources = []
+#0- Ontario, 1-Alberta, 2-Nova Scotia, 3-New Brunswick
+data_sources = ['http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet',
+				'http://reports.ieso.ca/public/GenOutputbyFuelHourly/',
+				'https://www.nspower.ca/oasis/monthly-reports/hourly-total-net-nova-scotia-load',
+				'https://tso.nbpower.com/Public/en/system_information_archive.aspx']
+
 def extractSource(data_sources, province):
+	String link = data_sources[province]
 	return 0
-	
+
 def scrapeAlberta(source):
 	return 0
 
@@ -20,4 +26,5 @@ def scrapeNS(source):
 	return 0
 
 def mainRun:
+	
 	return 0
