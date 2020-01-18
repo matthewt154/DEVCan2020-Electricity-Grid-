7fc82a123de187a524936cde74965d83c1757f5c
Matthew Tran <mtran014@uottawa.ca>
2020-01-18 13:44:03 -0800

    First commit

---
+++ MainScrapeScriptElectricLoad.py
@@ -0,0 +1,19 @@
+import requests 
+import bs4 
+import os
+#from apscheduler.schedulers 
+
+def scrapeAlberta:
+	return 0
+
+def scrapeOntario:
+	return 0
+
+def scrapeNB:
+	return 0
+
+def scrapeNS:
+	return 0
+
+def mainRun:
+	return 0
