ab934bd9b2de81ee656d59a370fab7e84642a430
Iyiola Aloko <iyi.aloko@gmail.com>
2020-01-18 17:55:39 -0500

    Update MainScrapeScriptElectricLoad.py

--- MainScrapeScriptElectricLoad.py
+++ MainScrapeScriptElectricLoad.py
@@ -1,9 +1,10 @@
 import requests 
 import bs4 
 import os
+import lxml.html as lh
 #from apscheduler.schedulers 
 
-#0- Ontario, 1-Alberta, 2-Nova Scotia, 3-New Brunswick
+#0- Alberta, 1-Ontario, 2-Nova Scotia, 3-New Brunswick
 data_sources = ['http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet',
 				'http://reports.ieso.ca/public/GenOutputbyFuelHourly/',
 				'https://www.nspower.ca/oasis/monthly-reports/hourly-total-net-nova-scotia-load',
@@ -15,6 +16,32 @@ def scrapeAlberta(source):
 	2-Go to AIL value and isolate it
 	3- Write it to correct spot in .csv file
 	'''
+	page = requests.get(source)
+	tree = lh.fromstring(page.content)
+	#get value of elements in td
+	td_elements = tree.xpath('//td/text()')
+	#storing the value in a list
+	l_td = []
+	for t in td_elements:
+		l_td.append(t)
+	#from our list, we get the value of the Alberta Internal Load
+	for i in range(len(l_td) - 1):
+		if l_td[i] == 'Alberta Internal Load (AIL)':
+			alberta_value = l_td[i + 1]
+	#Getting the time of last update for our value
+	for i in range(len(l_td) - 1):
+		if "Last Update" in l_td[i]:
+			format_last_update = l_td[i]
+	#Storing the data for the date of the last update of this information
+	full_date_of_last_update = format_last_update.split()
+	last_update_month = full_date_of_last_update[3]
+	last_update_date = full_date_of_last_update[4]
+	last_update_year = full_date_of_last_update[5]
+	last_update_time = full_date_of_last_update[6]
+	#Checking to see if there is a comma in the string for the date so that we remove
+	if "," in last_update_date:
+		last_update_date = last_update_date.replace(","," ")
+	print(alberta_value)
 	return 0
 
 def scrapeOntario(source):
@@ -24,7 +51,6 @@ def scrapeOntario(source):
 	3- Get value (TOTAL OUTPUT) from last column
 	4- Write to .csv file in correct column
 	'''
-	int value
 	 
 	return 0
 
@@ -45,6 +71,8 @@ def scrapeNS(source):
 	'''
 	return 0
 
-def mainRun:
+def mainRun():
 	
 	return 0
+
+scrapeAlberta(data_sources[0])
